dc1ad95b329b26e4d50f9f6a59bf620a50c61f94
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2024-05-02 10:39:15 +0700

    clone

--- odoo/addons/th_eteaching/models/th_block_combine.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ThAdmissionGroup(models.Model):
-    _name = 'th.block.combine'
-    _description = 'Tổ hợp xét tuyển'
-
-    name = fields.Char(string="Tổ hợp", required=True)
-    th_code = fields.Char(string="Mã tổ hợp", required=True)
